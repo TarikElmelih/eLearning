e32a3110e7e138a209e82b2923ce9300a96c5d5d
Mohammad Ali Alkhalaf <eng.mhalkhalaf@gmail.com>
2023-05-08 22:37:39 +0300

    Login

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-var progressBar = document.getElementsByClassName("width-border");
-var progressPercent = 50;
-progressBar.style.width = progressPercent + "%";
+var st = false
+let ShowLoginPas = document.querySelector('#showlogin')
+let ClosLoginPas = document.querySelector('#closlogin')
+
+function login() {
+    if (st) {
+        document.querySelector('#password').setAttribute('type', 'password');
+        ShowLoginPas.classList.add('d-none')
+        ClosLoginPas.classList.remove('d-none')
+        st = false
+    } else {
+        document.querySelector('#password').setAttribute('type', 'text');
+        ClosLoginPas.classList.add('d-none')
+        ShowLoginPas.classList.remove('d-none')
+        st = true
+    }
+}
